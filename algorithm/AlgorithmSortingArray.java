462a6ded6fe37b0b090ae133b59cdf72d0b2a55b
Nona Vrtanesyan <nona.vrtanesyan@mail.ru>
2020-06-13 17:49:46 +0400

    sort algorythm added

---
+++ algorithm/AlgorithmSortingArray.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+import java.util.Arrays;
+
+public class AlgorithmSortingArray {
+	
+	 public static void main (String [] args) {
+		   int [] array = {45,12,85,32,89,39,69,44,42,1,6,8};
+		   Arrays.sort(array);
+		   for (int i = 0; i < array.length; i++) {
+		   System.out.println(array[i]);
+		   };
+		  }
+
+}
